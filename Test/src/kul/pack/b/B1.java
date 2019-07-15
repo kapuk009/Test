0fde7f390247b23ec4046e3b543cc73bcd96edfd
songpholtan <songpholtan@GSSD_Songpholt.7eleven.cp.co.th>
2019-07-15 10:16:16 +0700

    Third Commit : 3# Branch One

---
+++ Test/src/kul/pack/b/B1.java
@@ -0,0 +1,11 @@
+package kul.pack.b;
+
+public class B1 {
+	public void editFirst() {
+		System.out.println("Edit First");
+	}
+	
+	public void editSecond() {
+		System.out.println("Edit FirSecond");
+	}
+}
